b0488d1dd1968ccd06bf6a8bdb488701d208cfc2
Suhas <suhas8706@gmail.com>
2024-02-21 10:37:19 +0530

    second

--- frontend/src/Pages/Cart.js
+++ frontend/src/Pages/Cart.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import './CSS/cart.css'
 const Cart = () => {
   return (
     <div>
-      CArt
+     <div className='Information'>
+             <table>
+              <tr>Products</tr>
+              <tr>title</tr>
+              <tr>price</tr>
+              <tr>Quantity</tr>
+              <tr>Total</tr>
+             </table>
+             <hr className='linee'/>
+     </div>
+     <div className='direction'>
+       <div className='CartTotals'>
+          <h1>Cart Totals</h1>
+          <p>Subtotal:</p>
+          <hr />
+          <p>Shipping:</p>
+          <hr/>
+          <h1 className='total'>Total:</h1>
+        </div>
+       <div>
+        <h1>Find in the store</h1>
+       </div>
+     </div>
     </div>
   )
 }
